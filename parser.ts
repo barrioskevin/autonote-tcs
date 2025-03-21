5717f5ba34cbb395af2417e057b757e57d61c24f
Kevin Barrios <kusakus@live.com>
2025-03-21 15:10:21 -0700

    formatting

--- parser.ts
+++ parser.ts
@@ -1,24 +1,24 @@
-import { promises as fs } from 'fs';
+import { promises as fs } from "fs";
 
 interface Project {
-  url: string,
-  login: string,
-  password: string,
+  url: string;
+  login: string;
+  password: string;
 }
 
 interface Note {
-  student_name: string,
-  new_app: boolean,
-  concepts: string[],
-  note: string,
-  language: string,
-  interest_level: string,
-  project?: Project,
-  app_name: string,
+  student_name: string;
+  new_app: boolean;
+  concepts: string[];
+  note: string;
+  language: string;
+  interest_level: string;
+  project?: Project;
+  app_name: string;
 }
 
 /**
- * parses the notes written from a text file to an array of 
+ * parses the notes written from a text file to an array of
  * Note objects.
  *
  * the file is expected to follow the provided template for writting notes.
@@ -28,15 +28,15 @@ interface Note {
  * a better way, maybe throwing the error excplicitly? but right now i just check for
  * an empty array when using this to ensure notes exists.
  *
- * @param fileName - path of file to be read by fs.readFile 
+ * @param fileName - path of file to be read by fs.readFile
  * @return Note[] array of successfully parsed notes.
  */
-export const parseNotes = async (fileName : string) => {
+export const parseNotes = async (fileName: string) => {
   console.log("Running note parser...");
-  const notes : Note[] = [];
-  try{
-    const text = await fs.readFile(fileName, 'utf-8');
-    const lines = text.split('\n');
+  const notes: Note[] = [];
+  try {
+    const text = await fs.readFile(fileName, "utf-8");
+    const lines = text.split("\n");
 
     let note_processed = false;
     let student_name = "";
@@ -44,87 +44,71 @@ export const parseNotes = async (fileName : string) => {
     let note = "";
     let language = "";
     let interest_level = "green";
-    let project : Project | undefined = undefined;
+    let project: Project | undefined = undefined;
     let app_name = "";
-    let concepts : string[] = [];
+    let concepts: string[] = [];
 
     lines.forEach((line, index) => {
-      if(line.includes("---New App For "))
-      {
+      if (line.includes("---New App For ")) {
         new_app = true;
-      }
-      else if(line.includes("---Language For "))
-      {
+      } else if (line.includes("---Language For ")) {
         const splits = line.split(" ");
         const threePartName = splits.length === 5;
-        student_name = threePartName ? (splits[2] + " " + splits[3] + " " + splits[4])
-                                     : (splits[2] + " " + splits[3]);
+        student_name = threePartName
+          ? splits[2] + " " + splits[3] + " " + splits[4]
+          : splits[2] + " " + splits[3];
         student_name = student_name.replace("---", "");
-        language = lines[index+1];
-      }
-      else if(line.includes("---App Name For "))
-      {
-        app_name = lines[index+1];
-      }
-      else if(line.includes("---Project Details For "))
-      {
-        const url = lines[index+1].replace("URL:", "");
-        const login = lines[index+2].replace("Login:", "");
-        const password = lines[index+3].replace("Password:", "");
-        if(url !== "" && login !== "" && password != "")
-        {
+        language = lines[index + 1];
+      } else if (line.includes("---App Name For ")) {
+        app_name = lines[index + 1];
+      } else if (line.includes("---Project Details For ")) {
+        const url = lines[index + 1].replace("URL:", "");
+        const login = lines[index + 2].replace("Login:", "");
+        const password = lines[index + 3].replace("Password:", "");
+        if (url !== "" && login !== "" && password != "") {
           project = {
             url: url,
             login: login,
             password: password,
-          }
+          };
         }
-      }
-      else if(line.includes("---Note For "))
-      {
+      } else if (line.includes("---Note For ")) {
         let track = 1;
-        let curr = lines[index+track];
-        while(curr !== "---end of note---")
-        {
+        let curr = lines[index + track];
+        while (curr !== "---end of note---") {
           note += curr;
           track += 1;
-          curr = lines[index+track];
-          note += '\n';
+          curr = lines[index + track];
+          note += "\n";
         }
-      }
-      else if(line.includes("---Concepts For "))
-      {
-        concepts = lines[index+1].split(",").map(concept => concept.trim());
-      }
-      else if(line.includes("---end of concepts---"))
-      {
+      } else if (line.includes("---Concepts For ")) {
+        concepts = lines[index + 1].split(",").map((concept) => concept.trim());
+      } else if (line.includes("---end of concepts---")) {
         note_processed = true;
       }
 
-      if(note_processed)
-      {
-        const new_note : Note = project === undefined 
-        ?
-        ({
-          student_name: student_name,
-          new_app: new_app,
-          concepts: [...concepts],
-          note: note,
-          language: language,
-          interest_level: interest_level,
-          app_name: app_name,
-        })
-        :
-        ({
-          student_name: student_name,
-          new_app: new_app,
-          concepts: [...concepts],
-          note: note,
-          language: language,
-          project: project,
-          interest_level: interest_level,
-          app_name: app_name,
-        });
+      if (note_processed) {
+        const new_note: Note =
+          project === undefined
+            ? {
+                student_name: student_name,
+                new_app: new_app,
+                concepts: [...concepts],
+                note: note,
+                language: language,
+                interest_level: interest_level,
+                app_name: app_name,
+              }
+            : {
+                student_name: student_name,
+                new_app: new_app,
+                concepts: [...concepts],
+                note: note,
+                language: language,
+                project: project,
+                interest_level: interest_level,
+                app_name: app_name,
+              };
         notes.push(new_note);
         note_processed = false;
         student_name = "";
@@ -138,8 +122,7 @@ export const parseNotes = async (fileName : string) => {
       }
     });
     console.log("Parser completed...");
-  } 
-  catch(error){
+  } catch (error) {
     console.log("Error in note parsing!!");
     console.log(error);
   }
--- submitter.js
+++ submitter.js
@@ -1,17 +1,15 @@
-const { chromium } = require('playwright');
-const { parseNotes } = require('./parser');
-require('dotenv').config();
+const { chromium } = require("playwright");
+const { parseNotes } = require("./parser");
+require("dotenv").config();
 
 //read credentials from enviornment variables.
 const PIKE_EMAIL = process.env.PIKE_EMAIL;
 const PIKE_PASSWORD = process.env.PIKE_PASSWORD;
-if(!PIKE_EMAIL)
-{
+if (!PIKE_EMAIL) {
   console.log("NO EMAIL DEFINED, CHECK YOUR .env");
   process.exit(1);
 }
-if(!PIKE_PASSWORD)
-{
+if (!PIKE_PASSWORD) {
   console.log("NO PASSWORD DEFINED, CHECK YOUR .env");
   process.exit(1);
 }
@@ -20,7 +18,8 @@ const MC = {
   url: "https://server.thecoderschool.com/toolset/",
   email: PIKE_EMAIL,
   password: PIKE_PASSWORD,
-  pikeSignIn: "a[href='https://pike13.com/oauth/authorize?client_id=4xSEtgehj8RCA30riFUo96CtA8LGJ4lhkxWEYHVU&response_type=code&redirect_uri=https%3A%2F%2Fserver.thecoderschool.com%2Ftoolset%2F']",
+  pikeSignIn:
+    "a[href='https://pike13.com/oauth/authorize?client_id=4xSEtgehj8RCA30riFUo96CtA8LGJ4lhkxWEYHVU&response_type=code&redirect_uri=https%3A%2F%2Fserver.thecoderschool.com%2Ftoolset%2F']",
   pikeEmail: "#account_email",
   pikePassword: "#account_password",
   todoColor: "rgb(0, 170, 0)",
@@ -29,13 +28,13 @@ const MC = {
 };
 
 //makes call to the parser script
-//returns array of Note[] 
+//returns array of Note[]
 const fetchNotes = async (fileName) => {
   return await parseNotes(fileName);
-}
+};
 
 const fillNotes = async (notes, todaysDate) => {
-  // debug mode. 
+  // debug mode.
   const DEBUG = false;
 
   // setup browser & context
@@ -45,58 +44,58 @@ const fillNotes = async (notes, todaysDate) => {
   //open the sign in page & sign in.
   const page = await context.newPage();
   await page.goto(MC.url);
-  await page.getByPlaceholder('Email address').fill(MC.email);
-  await page.getByPlaceholder('Password').fill(MC.password);
-  await page.getByRole('button', {name: 'Sign In'}).click();
-  await page.getByRole('button', {name: 'Allow Access'}).click();
+  await page.getByPlaceholder("Email address").fill(MC.email);
+  await page.getByPlaceholder("Password").fill(MC.password);
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByRole("button", { name: "Allow Access" }).click();
 
-  
   //open notes page - newPage
   const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    page.getByRole('button', {name: 'Notes+'}).click(),
+    context.waitForEvent("page"),
+    page.getByRole("button", { name: "Notes+" }).click(),
   ]);
-  await newPage.waitForLoadState('domcontentloaded');
+  await newPage.waitForLoadState("domcontentloaded");
 
   //get todays div and extract the noteButtons
   const todaysLocator = await newPage.getByText(todaysDate);
   const locatorCheck = await todaysLocator.count();
-  if(locatorCheck === 0)
-  {
+  if (locatorCheck === 0) {
     console.log(`[ERROR] Couldnt find notes for provided date: ${todaysDate}`);
     await browser.close();
     return;
   }
-  const noteButtons = todaysLocator.locator('button.notebutton');
+  const noteButtons = todaysLocator.locator("button.notebutton");
 
   //use the noteButtons to get the names.
   const N = await noteButtons.count();
   const studentNames = [];
   const name_to_buttons = new Map();
 
-  for(let i = 0; i < N; i++)
-  {
-    const content = await noteButtons.nth(i).textContent(); 
-    const splits = content.split(' ');
+  for (let i = 0; i < N; i++) {
+    const content = await noteButtons.nth(i).textContent();
+    const splits = content.split(" ");
     const time = splits[0];
     let fnIdx = 3;
     let lnIdx = 4;
-    if(splits.length === 4)
-    {
+    if (splits.length === 4) {
       fnIdx -= 1;
       lnIdx -= 1;
     }
-    const name = (splits[fnIdx] + " " + splits[lnIdx] + (splits.length === 6 ? (" " + splits[5]) : ""))
-                 .replace("Session", "")
-                 .replace("Coaching", "")
-                 .replace("Class", "")
-                 .trim()
-                 .toLowerCase();
+    const name = (
+      splits[fnIdx] +
+      " " +
+      splits[lnIdx] +
+      (splits.length === 6 ? " " + splits[5] : "")
+    )
+      .replace("Session", "")
+      .replace("Coaching", "")
+      .replace("Class", "")
+      .trim()
+      .toLowerCase();
     studentNames.push(name);
     name_to_buttons.set(name, noteButtons.nth(i));
 
-    if(DEBUG)
-    {
+    if (DEBUG) {
       console.log("DEBUG: found note name on page: ", name);
       console.log("DEBUG: full split: ", splits);
       console.log("DEBUG: full content: ", content);
@@ -104,35 +103,28 @@ const fillNotes = async (notes, todaysDate) => {
   }
 
   //check note colors
-  for(let i = 0; i < N; i++)
-  {
-    const color = await noteButtons.nth(i).evaluate(button => {
+  for (let i = 0; i < N; i++) {
+    const color = await noteButtons.nth(i).evaluate((button) => {
       return window.getComputedStyle(button).backgroundColor.trim();
     });
-    if(color !== 'rgb(0, 170, 0)')
-    {
+    if (color !== "rgb(0, 170, 0)") {
       name_to_buttons.delete(studentNames[i]);
     }
   }
 
   const studentPages = [];
-  for(const [k, v] of name_to_buttons)
-  {
+  for (const [k, v] of name_to_buttons) {
     const [student_page] = await Promise.all([
-      context.waitForEvent('page'),
+      context.waitForEvent("page"),
       v.click(),
     ]);
     studentPages.push({ name: k, page: student_page });
   }
 
-
-  if(studentPages.length > 0)
-  {
+  if (studentPages.length > 0) {
     console.log("Students that need notes:");
-    studentPages.forEach(sp => console.log(`\t${sp.name}`));
-  }
-  else
-  {
+    studentPages.forEach((sp) => console.log(`\t${sp.name}`));
+  } else {
     console.log("Found no students that need notes!");
     console.log("if this is not expected check date and/or duplicate names");
     await browser.close();
@@ -141,138 +133,138 @@ const fillNotes = async (notes, todaysDate) => {
 
   const AMT_NEEDED = studentPages.length > 0 ? studentPages.length : 0;
 
-  const valid_languages = ["Scratch", "Python", "HTML/CSS", "Javascript", "Java", "C#"];
+  const valid_languages = [
+    "Scratch",
+    "Python",
+    "HTML/CSS",
+    "Javascript",
+    "Java",
+    "C#",
+  ];
   let filled = 0;
-  await Promise.all(studentPages.map(async ({name, page}) => {
-    console.log("Writing note for: ", name);
+  await Promise.all(
+    studentPages.map(async ({ name, page }) => {
+      console.log("Writing note for: ", name);
 
-    //extract student's note.
-    const note = notes.find(n => n.student_name === name);
-    if(!note)
-    {
-      console.log("No corresponding note found for ", name);
-      await page.close();
-      return;
-    }
-
-    await page.waitForLoadState('load');
-
-    //verify name's match.
-    const pn = await page.title();
-    const page_name = pn.replace(" Notes+", "").toLowerCase();
-    if(!(name === note.student_name && name === page_name))
-    {
-      console.log("couldn't match the name to page title for ", name);
-      console.log(name, " does not equal ", note.student_name);
-      console.log("OR");
-      console.log(name, " does not equal ", page_name);
-      await page.close();
-      return;
-    }
-
-    //check for scratch warning.
-    const popupLocator = page.locator("#pop-content5");
-    const scratchWarning = await popupLocator.isVisible();
-    if(scratchWarning)
-    {
-      const closeButton = page.locator("#popclose5");
-      await closeButton.click();
-    }
-
-    //start new app if applicable.
-    if(note.new_app)
-    {
-      const newAppButton = page.locator("a:has-text('start a new app')");
-      const isVisible = await newAppButton.isVisible();
-      if(isVisible)
-      {
-        await newAppButton.click();
-        await page.waitForLoadState('load');
+      //extract student's note.
+      const note = notes.find((n) => n.student_name === name);
+      if (!note) {
+        console.log("No corresponding note found for ", name);
+        await page.close();
+        return;
       }
-    }
 
-    //set the language.
-    const languageSelector = page.locator('#language');
-    if(valid_languages.includes(note.language))
-    {
-      await languageSelector.selectOption(note.language);
-    }
-    else
-    {
-      await languageSelector.selectOption('Other');
-      const otherInput = page.locator('#otherlang');
-      await otherInput.fill(note.language);
-    }
+      await page.waitForLoadState("load");
 
-    //set app name
-    const appNameInput = page.locator('#appname');
-    await appNameInput.fill(note.app_name);
+      //verify name's match.
+      const pn = await page.title();
+      const page_name = pn.replace(" Notes+", "").toLowerCase();
+      if (!(name === note.student_name && name === page_name)) {
+        console.log("couldn't match the name to page title for ", name);
+        console.log(name, " does not equal ", note.student_name);
+        console.log("OR");
+        console.log(name, " does not equal ", page_name);
+        await page.close();
+        return;
+      }
 
-    //fill login
-    if(note.project)
-    {
-      const urlInput = page.locator('#url');
-      const loginInput = page.locator('#login');
-      const passwordInput = page.locator('#pwd');
+      //check for scratch warning.
+      const popupLocator = page.locator("#pop-content5");
+      const scratchWarning = await popupLocator.isVisible();
+      if (scratchWarning) {
+        const closeButton = page.locator("#popclose5");
+        await closeButton.click();
+      }
 
-      await urlInput.fill(note.project.url);
-      await loginInput.fill(note.project.login);
-      await passwordInput.fill(note.project.password);
-    }
+      //start new app if applicable.
+      if (note.new_app) {
+        const newAppButton = page.locator("a:has-text('start a new app')");
+        const isVisible = await newAppButton.isVisible();
+        if (isVisible) {
+          await newAppButton.click();
+          await page.waitForLoadState("load");
+        }
+      }
 
-    //fill concepts
-    const conceptInput = page.locator('#addconcept');
-    const addConceptButton = page.locator('button.gobutton:has-text("Add")');
-    for(const concept of note.concepts)
-    {
-      await conceptInput.fill(concept);
-      await addConceptButton.click();
-    }
+      //set the language.
+      const languageSelector = page.locator("#language");
+      if (valid_languages.includes(note.language)) {
+        await languageSelector.selectOption(note.language);
+      } else {
+        await languageSelector.selectOption("Other");
+        const otherInput = page.locator("#otherlang");
+        await otherInput.fill(note.language);
+      }
 
-    //fill in the note.
-    const note_iframe = page.frameLocator('iframe.tox-edit-area__iframe');
-    const note_body = note_iframe.locator('body');
-    await note_body.pressSequentially(note.note);
+      //set app name
+      const appNameInput = page.locator("#appname");
+      await appNameInput.fill(note.app_name);
 
-    //fill in interest level.
-    const interestSelector = page.locator('#studentinterest');
-    await interestSelector.selectOption(note.interest_level);
+      //fill login
+      if (note.project) {
+        const urlInput = page.locator("#url");
+        const loginInput = page.locator("#login");
+        const passwordInput = page.locator("#pwd");
 
-    //submit the note.
-    const submitButton = page.locator('input[type="submit"]');
-    await submitButton.click();
-    await page.waitForLoadState('load');
+        await urlInput.fill(note.project.url);
+        await loginInput.fill(note.project.login);
+        await passwordInput.fill(note.project.password);
+      }
 
-    //NEED TO CHECK IF ACTUALLY GOT SUMBITTED HERE!
-    
-    //close tab.
-    console.log("Successfully submitted note for ", note.student_name);
-    await page.close();
-    filled += 1;
-  }));
+      //fill concepts
+      const conceptInput = page.locator("#addconcept");
+      const addConceptButton = page.locator('button.gobutton:has-text("Add")');
+      for (const concept of note.concepts) {
+        await conceptInput.fill(concept);
+        await addConceptButton.click();
+      }
+
+      //fill in the note.
+      const note_iframe = page.frameLocator("iframe.tox-edit-area__iframe");
+      const note_body = note_iframe.locator("body");
+      await note_body.pressSequentially(note.note);
+
+      //fill in interest level.
+      const interestSelector = page.locator("#studentinterest");
+      await interestSelector.selectOption(note.interest_level);
+
+      //submit the note.
+      const submitButton = page.locator('input[type="submit"]');
+      await submitButton.click();
+      await page.waitForLoadState("load");
+
+      //NEED TO CHECK IF ACTUALLY GOT SUMBITTED HERE!
+
+      //close tab.
+      console.log("Successfully submitted note for ", note.student_name);
+      await page.close();
+      filled += 1;
+    }),
+  );
 
   //RIGHT NOW THE FILLEd COUNT MY NOT BE ACCURATE!
-  console.log("Filled ", filled, " notes out of ", AMT_NEEDED); 
+  console.log("Filled ", filled, " notes out of ", AMT_NEEDED);
   await browser.close();
 };
 
 const run = async () => {
-
   const argc = process.argv.length;
-  if(argc !== 4 && argc !== 5)
-  {
+  if (argc !== 4 && argc !== 5) {
     console.log("Usage:");
     console.log(" $ npm run submit -- 'path/to/note.txt'");
     console.log("\tOR");
     console.log(" $ npm run submit -- 'date' 'path/to/note.txt'\n");
-    console.log("NOTE: Date should be provided 'mm/dd'. Omitting the date and only providing the file will try to submit notes for the current day.\n");
-    console.log("Example of submitting a note for March 20th with a note file from a notes folder in the current directory.");
+    console.log(
+      "NOTE: Date should be provided 'mm/dd'. Omitting the date and only providing the file will try to submit notes for the current day.\n",
+    );
+    console.log(
+      "Example of submitting a note for March 20th with a note file from a notes folder in the current directory.",
+    );
     console.log(" $ npm run submit -- '03/20' 'notes/note_0320.txt'");
     process.exit(1);
   }
   const noteFileName = argc === 4 ? process.argv[3] : process.argv[4];
-  if(!noteFileName.toLowerCase().endsWith('.txt'))
-  {
+  if (!noteFileName.toLowerCase().endsWith(".txt")) {
     console.log(`Error reading file name: ${noteFileName}`);
     process.exit(1);
   }
@@ -280,32 +272,32 @@ const run = async () => {
   //dates will always be updated to the current year!
   const noteDate = argc === 4 ? new Date() : new Date(process.argv[3]);
   noteDate.setFullYear(new Date().getFullYear());
-  if(isNaN(noteDate.getTime()))
-  {
+  if (isNaN(noteDate.getTime())) {
     console.log(`Error reading entered date: ${process.argv[3]}`);
     process.exit(1);
   }
   console.log(noteDate);
-  const noteDateString = noteDate.toLocaleDateString("en-US", { 
-        weekday: "short", 
-        month: "short", 
-        day: "numeric",
-  }).replace(",", "");
+  const noteDateString = noteDate
+    .toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    })
+    .replace(",", "");
 
   console.log("Running autonote note submitter...");
   console.log(`Using file: ${noteFileName}`);
   console.log(`Using date: ${noteDateString}`);
 
   const notes = await fetchNotes(noteFileName);
-  if(notes.length === 0)
-  {
+  if (notes.length === 0) {
     console.log("No notes found from file + parser.");
     process.exit(1);
   }
 
   console.log(`${notes.length} notes found`);
-  console.log('Students (found in file):');
-  notes.forEach(note => console.log(`\t${note.student_name}`));
+  console.log("Students (found in file):");
+  notes.forEach((note) => console.log(`\t${note.student_name}`));
 
   await fillNotes(notes, noteDateString);
 
